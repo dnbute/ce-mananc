e0267f256a36215ee004697273a4bee55fd03cf6
dnbute <bute.dragos.cristian@gmail.com>
2021-04-11 17:44:57 +0300

    Reads recipes eaten before and picks a random new recipe

--- database_handler.py
+++ database_handler.py
@@ -1,16 +1,19 @@
 import os
 import json
+import random
 
 class DatabaseHandler:
-    def __init__(self, path, num_days):
-        self.path = path
+    def __init__(self, path_retete, path_last_days, num_days):
+        self.path_retete = path_retete
+        self.path_last_days = path_last_days
         self.recipes = []
         self.num_days = num_days
         self.last_days = []
+        random.seed()
     
     def read_recipes(self):
-        path_name = os.path.join(os.getcwd(), self.path)
-        files = os.listdir(os.path.join(os.getcwd(), self.path))
+        path_name = os.path.join(os.getcwd(), self.path_retete)
+        files = os.listdir(os.path.join(os.getcwd(), self.path_retete))
         for filename in files:
             with open(os.path.join(path_name, filename), 'r') as f:
                 data = json.load(f)
@@ -19,7 +22,25 @@ class DatabaseHandler:
     def print_recipes(self):
         for recipe in self.recipes:
             print("nume = {}, ingredient = {}".format(recipe["nume"], recipe["ingredient"]))
+    
+    def pick_random_recipe(self):
+        self.chosen_recipe = self.recipes[random.randint(0, len(self.recipes) - 1)]
+        print("Chosen recipe {}".format(self.chosen_recipe["nume"]))
 
     def read_last_days(self):
-        pass
+        path_name = os.path.join(os.getcwd(), self.path_last_days)
+        files = os.listdir(os.path.join(os.getcwd(), self.path_last_days))
+        for filename in files:
+            with open(os.path.join(path_name, filename), 'r') as f:
+                data = json.load(f)
+                self.last_days.append(data)
+    
+    def remove_eaten_recipes(self):
+        for eaten_recipe in self.last_days:
+            self.recipes.remove(eaten_recipe)
+
+    def write_chosen_recipe(self):
+        path_name = os.path.join(os.getcwd(), self.path_last_days)
+        with open(os.path.join(path_name, self.chosen_recipe["nume"] + '.json'), 'w') as f:
+            json.dump(self.chosen_recipe, f, indent=4)
 
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import database_handler
 
 
 def main(argv):
-    if len(argv) != 2:
-        print("Please provide the path for the recipes")
-    print("path = " + argv[1])
-    database = database_handler.DatabaseHandler(argv[1], 4)
+    if len(argv) != 3:
+        print("Please provide the path for the recipes and for the last n days")
+    print("recipe path = " + argv[1])
+    print("database path = " + argv[2])
+    database = database_handler.DatabaseHandler(argv[1], argv[2], 4)
     database.read_recipes()
+    database.read_last_days()
+    database.remove_eaten_recipes()
     database.print_recipes()
+    database.pick_random_recipe()
+    database.write_chosen_recipe()
    
     
 
